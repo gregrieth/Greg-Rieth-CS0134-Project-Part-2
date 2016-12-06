5aeff714256e8df589da1ea6fc29dc8128a2ae2e
gregrieth <user@c-24-3-140-30.hsd1.pa.comcast.net>
2016-12-05 23:17:25 -0500

    Greg Rieth-Project Part 2-CS0134

---
+++ jquery-3.1.1.js
@@ -0,0 +1,8 @@
+<div class="gallery">
+          <div><img src="../Images/fridge image.png" alt="" title="Refrigerator"></div>
+          <div><img src="../Images/gas range image.png" alt="" title="Gas Range"></div>
+          <div><img src="../Images/sofa image.png" alt="" title="Leather Sofa"></div>
+          <div><img src="../Images/dining room set image.png" alt="" title="Dining Room Set"></div>
+          <div><img src="../Images/desk image.png" alt="" title="Desk"></div>
+          <div><img src="../Images/bed image.png" alt="" title="Queen-Sized Bed"></div>
+        </div>
